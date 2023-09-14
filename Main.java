7021f57484d724440952b2be848d2a78f902a32b
nadhellabharathi <51709791+nadhellabharathi@users.noreply.github.com>
2023-09-14 12:03:41 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int sum=0;
+        for(int i=0;i<10;i++){
+            System.out.println("Hai");
+            System.out.println(sum+i);
+        }
+    }
+}
